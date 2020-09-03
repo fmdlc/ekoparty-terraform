62d95d800bdf8913eee537f601cc3a8b2437dce7
Facu de la Cruz <fmdlc.unix@gmail.com>
2020-09-03 03:33:48 -0300

    Updating configuration to use remote state in Terraform Cloud
    Create terraform.yml
    
    Update terraform.yml
    
    Update terraform.yml
    
    Updating variables default values
    
    Update terraform.yml
    
    Update terraform.yml
    
    Update terraform.yml
    
    Update terraform.yml
    
    Update terraform.yml
    
    Update terraform.yml
    
    Enabled actions/github-script and tflint
    
    Adding Terraform Apply Action
    
    Update terraform-plan.yml
    
    Create terraform-apply.yml
    
    Update terraform-plan.yml
    
    Update terraform-apply.yml
    
    Update terraform-plan.yml
    
    Update terraform-apply.yml
    
    Update terraform-plan.yml
    
    Update terraform-plan.yml
    
    Enabling new build structure

---
+++ backend.tf
@@ -0,0 +1,15 @@
+#------------------------------------------------------------------------------
+# AWS Backend configuration
+#
+#------------------------------------------------------------------------------
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "tty0"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "ekoparty-devsecops"
+    }
+  }
+}
