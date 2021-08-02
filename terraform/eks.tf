06a81a9b8c36e3872b4eadc025e6adfc778e6860
Facu de la Cruz <fmdlc.unix@gmail.com>
2021-08-02 17:09:11 -0300

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,12 +1,12 @@
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.cluster.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#  token                  = data.aws_eks_cluster_auth.cluster.token
-#}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
 module "eks-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "ekoparty"
+  cluster_name    = "demo-cluster"
   cluster_version = "1.20"
   subnets         = [for subnet in module.vpc.public_subnets : subnet]
   vpc_id          = module.vpc.vpc_id
